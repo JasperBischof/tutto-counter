28770507084d3fd461812c46ccfeb4b2d703854d
JasperBischof <kontakt@jasperbischof.de>
2026-08-26 17:18:24 +0200

    added pressed states to keypad

--- src/components/ui_elements/Keypad.vue
+++ src/components/ui_elements/Keypad.vue
@@ -12,16 +12,18 @@ const NUMBER_KEYS = ["7", "8", "9", "4", "5", "6", "1", "2", "3"];
   <section id="keypad" class="flex h-64">
     <div class="grid grid-cols-3 grid-rows-4 gap-1 w-full h-full">
       <button
-        class="border rounded"
+        class="border rounded active:bg-slate-200"
         v-for="key in NUMBER_KEYS"
         :key="key"
         @click="onDigit(key)"
       >
         {{ key }}
       </button>
-      <button class="border rounded" @click="onDigit('0')">0</button>
+      <button class="border rounded active:bg-slate-200" @click="onDigit('0')">
+        0
+      </button>
       <button
-        class="col-span-2 border rounded"
+        class="col-span-2 border rounded active:bg-slate-200"
         @click="onSubmit"
         :class="{ submitDisabled }"
       >
